f0609a794d9116d8cc44e9f5e8ba3f427525e81c
Alex Nor <advance114@gmail.com>
2025-07-08 17:47:03 +0300

    Chapter 5 C# and visual programming

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
