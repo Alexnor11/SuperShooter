5366714321d795bd028baaf925f7730461509961
Alex Nor <advance114@gmail.com>
2025-11-06 21:48:14 +0300

    Adding graphs

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speed);
+        }  
+        if (Input.GetKey(KeyCode.S)) { transform.Translate(0, 0, -speed);}
+        if (Input.GetKey(KeyCode.A)) { transform.Translate(-speed, 0, 0);}
+        if (Input.GetKey(KeyCode.D)) {transform.Translate(speed, 0, 0);}
+    }
+}
