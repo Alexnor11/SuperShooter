1dad3421848e5ae1d811ff62d13ca0a86a8f6b8d
Alex Nor <advance114@gmail.com>
2025-11-26 15:43:17 +0300

    Scheduled actions 0.1

---
+++ Assets/_Scripts/WaveSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float startTime;
+    public float endTime;
+    public float spawnRate;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", startTime, spawnRate);
+        Invoke("CancelInvoke", endTime);
+    }
+
+    void Spawn()
+    {
+        Instantiate(prefab, transform.position,transform.rotation);
+    }
+}
